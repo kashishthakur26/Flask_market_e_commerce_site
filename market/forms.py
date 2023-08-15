9dcb7b48e915d3d7380c91f2f4c21c95d5b7b79f
kashishthakur26 <kashishthakur.official.0907@gmail.com>
2023-08-15 14:45:51 +0530

    second commit

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-
+from wtforms.validators import Length ,EqualTo , Email , DataRequired
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name:')
+    username = StringField(label='User Name:',validators=Length(min=2,max=30))
     email_address = StringField(label='Email Address:')
-    password1 = PasswordField(label='Password:')
+    password1 = PasswordField(label='Password:', validators = Length(min=2,max=7))
     password2 = PasswordField(label='Confirm Password:')
     submit = SubmitField(label='Create Account')
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,10 @@
+from joblib import register_parallel_backend
 from market import app
 from flask import render_template
-from market.models import Item
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
+from flask import url_for,redirect 
 
 @app.route('/')
 @app.route('/home')
@@ -13,7 +16,17 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password_hash=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if form.erros != {}:
+        for err_msg in form.errors.values():
+            print(f'There was an error { err_msg}')
     return render_template('register.html', form=form)
